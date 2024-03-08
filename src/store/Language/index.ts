f4f46e48dac64280333204270118291e257cc96b
GeorgeZhang2035 <goerge.zhang@silksoftware.com>
2024-03-08 18:01:47 +0800

    feat: store

--- src/store/Language/index.ts
+++ src/store/Language/index.ts
@@ -3,4 +3,10 @@ import { LANGUAGES } from "../../hooks"
 
 export const a = 1
 
-const languageCoreAtom = atom('language', LANGUAGES.zh)
+const languageCoreAtom = atom(LANGUAGES.zh)
+
+
+export const languageAtom = atom(
+    (get) => get(languageCoreAtom),
+    (get, set, newValue: LANGUAGES) => set(languageCoreAtom, newValue)
+)
