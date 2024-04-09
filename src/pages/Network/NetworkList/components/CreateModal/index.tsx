fa6b863566f71551fffbe5bb2ccd16b587de719c
GeorgeZhang2035 <goerge.zhang@silksoftware.com>
2024-04-09 17:49:51 +0800

    feat: create module compeleted 70%

--- src/pages/Network/NetworkList/components/CreateModal/index.tsx
+++ src/pages/Network/NetworkList/components/CreateModal/index.tsx
@@ -1,6 +1,6 @@
 import { useLanguageContext } from "@/hooks";
-import { Button, Modal } from "antd";
-import { FC, useState } from "react";
+import { Button, Flex, Form, Input, Modal } from "antd";
+import { Dispatch, FC, SetStateAction, useState } from "react";
 
 export interface CreateModalProp {}
 
@@ -17,9 +17,51 @@ export const CreateModal: FC<CreateModalProp> = () => {
         okText={LanguageText.confirm}
         cancelText={LanguageText.cancel}
         onCancel={() => setIsOpen(false)}
+        title={LanguageText.createTitle}
+        footer={false}
+        destroyOnClose
       >
-        111
+        <CreateModalMaintain setOpen={setIsOpen} />
       </Modal>
     </>
+  );
+};
+
+interface CreateModalMaintainProp {
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+interface FormItemProtocol {
+  name: string;
+  [key: string]: unknown;
+}
+
+const CreateModalMaintain: FC<CreateModalMaintainProp> = ({ setOpen }) => {
+  const { LanguageText } = useLanguageContext<"Network">();
+  const [form] = Form.useForm<FormItemProtocol>();
+  const [formValue, setFormValue] = useState<FormItemProtocol>()
+
+  const handleFinish = () => {
+    console.log(formValue)
+  }
+
+  return (
+    <Form form={form} layout="vertical" onFinish={handleFinish} onValuesChange={(newValue) => setFormValue(newValue)}>
+      <Form.Item<FormItemProtocol>
+        name="name"
+        required
+        rules={[{required: true}]}
+        label={LanguageText.nameItem}
+      >
+        <Input style={{ width: "60%" }} />
+      </Form.Item>
+      <Form.Item>uavs setting</Form.Item>
+      <Form.Item>
+        <Flex align="center" justify="end" gap={5}>
+          <Button onClick={() => setOpen(false)}>{LanguageText.cancel}</Button>
+          <Button type="primary" htmlType="submit">{LanguageText.confirm}</Button>
+        </Flex>
+      </Form.Item>
+    </Form>
   );
 };
