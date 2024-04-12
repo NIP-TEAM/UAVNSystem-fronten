d31e845ab707a761341ba7ab50a7a13accb176c4
GeorgeZhang2035 <goerge.zhang@silksoftware.com>
2024-04-12 16:15:00 +0800

    temp storage

--- src/pages/Uav/CreateUav/components/CreateUavForm/index.tsx
+++ src/pages/Uav/CreateUav/components/CreateUavForm/index.tsx
@@ -1,9 +1,56 @@
-import { Form } from "antd";
+import { Button, Form, Input } from "antd";
 import { FC } from "react";
 
 export interface CreateUavFormProp {}
 
-export const CreateUavForm: FC<CreateUavFormProp> = () => {
-    const [form] = Form.useForm()
-    return <Form form={form}>111</Form>
+interface FormItem {
+  netWorkId: number;
+  uavItem: unknown;
 }
+
+interface Form extends Record<"uavGroups", FormItem> {}
+
+export const CreateUavForm: FC<CreateUavFormProp> = () => {
+  const [form] = Form.useForm<FormItem[]>();
+  return (
+    <Form form={form}>
+      <Form.List name="uavGroups">
+        {(groupFields, { add: addGroup, remove: removeGroup }) => (
+          <>
+            {groupFields.map(
+              (
+                { key: groupKey, name: groupName, ...restGroupfeild },
+                groupIndex
+              ) => (
+                <Form.List name="uavItem" key={groupKey} {...restGroupfeild}>
+                  {(uavFields, { add: addUav, remove: removeUav }) => (
+                    <>
+                      {uavFields.map(
+                        (
+                          { key: uavKey, name: uavName, ...restUavFeild },
+                          uavIndex
+                        ) => (
+                          <>
+                            <Form.Item key={uavKey}>
+                              <Input />
+                            </Form.Item>
+                          </>
+                        )
+                      )}
+                      <Button onClick={addUav}>111</Button>
+                    </>
+                  )}
+                </Form.List>
+              )
+            )}
+            <Form.Item>
+              <Button type="primary" onClick={addGroup}>
+                添加
+              </Button>
+            </Form.Item>
+          </>
+        )}
+      </Form.List>
+    </Form>
+  );
+};
