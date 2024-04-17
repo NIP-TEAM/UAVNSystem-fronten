288b02b12053abf729d4317fcb470e2d7585c21f
GeorgeZhang2035 <goerge.zhang@silksoftware.com>
2024-04-17 17:44:03 +0800

    feat: feature list completed

--- src/pages/Network/NetworkProtocol/components/ProtocolList/components/Feature/index.tsx
+++ src/pages/Network/NetworkProtocol/components/ProtocolList/components/Feature/index.tsx
@@ -1,10 +1,28 @@
 import { FC } from "react";
 import { ProtocolDataType } from "@/service/Network";
+import { Empty, Flex } from "antd";
 
 export interface FeatureProp {
-    featureList: ProtocolDataType["feature"]
+  featureList: ProtocolDataType["feature"];
 }
 
-export const Feature: FC<FeatureProp> = () => {
-    return <div>111</div>
-}
+export const Feature: FC<FeatureProp> = ({ featureList }) => {
+    // TODO: build this later
+  return (
+    <Flex
+      style={{ width: "100%", height: 128, overflowY: "auto" }}
+      wrap="wrap"
+      justify="flex-start"
+    >
+      {featureList.length ? (
+        featureList.map((item) => (
+          <div key={item} style={{ width: "50%", padding: "0 0.5em" }}>
+            {item}
+          </div>
+        ))
+      ) : (
+        <Empty />
+      )}
+    </Flex>
+  );
+};
