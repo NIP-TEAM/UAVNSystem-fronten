3459331b99355e117665ca8bb1b855e4a3bf9e01
GeorgeZhang2035 <goerge.zhang@silksoftware.com>
2024-05-09 17:55:20 +0800

    temp storage

--- src/pages/User/components/InfoShowcase/index.tsx
+++ src/pages/User/components/InfoShowcase/index.tsx
@@ -1,7 +1,8 @@
+import { useLanguageContext } from "@/hooks";
 import { UserInfo } from "@/store";
 import { UserOutlined } from "@ant-design/icons";
-import { Avatar, Flex, Form, FormProps } from "antd";
-import { FC, useMemo, useState } from "react";
+import { Avatar, Button, Flex, Form, FormProps, Input, Typography } from "antd";
+import { FC, ReactNode, useMemo, useState } from "react";
 
 export interface InfoShowcaseProp {
   form: FormProps["form"];
@@ -12,14 +13,51 @@ export const InfoShowcase: FC<InfoShowcaseProp> = ({
   form,
   userInfo: { name, id, email, lastLogin, active },
 }) => {
+  const { LanguageText } = useLanguageContext<"User">();
+
   const editable = useMemo(() => !!form, [form]);
   const [edit, setEdit] = useState(false);
 
   return (
-    <Flex align="center" justify="center" style={{ height: "40em" }}>
-      <Form form={form}>
+    <Form form={form} onFinish={() => console.log("111")}>
+      <Flex align="center" justify="center" style={{ height: "40em" }} vertical>
         <Avatar icon={<UserOutlined />} size={384} />
-      </Form>
-    </Flex>
+
+        <FormItem name={LanguageText.nameLabel}>
+          {edit ? (
+            <Form.Item<UserInfo["name"]> name="name">
+              <Input allowClear />
+            </Form.Item>
+          ) : (
+            <Typography.Text>{name}</Typography.Text>
+          )}
+        </FormItem>
+      </Flex>
+      {editable && (
+        <Flex justify="end" style={{ marginBottom: 5 }} gap={"small"}>
+          <Button
+            type={edit ? "default" : "primary"}
+            onClick={() => setEdit((prev) => !prev)}
+          >
+            {edit ? LanguageText.editCancel : LanguageText.editButton}
+          </Button>
+          {edit && (
+            <Button type="primary" htmlType="submit">
+              {LanguageText.editConfirm}
+            </Button>
+          )}
+        </Flex>
+      )}
+    </Form>
   );
 };
+
+const FormItem: FC<{ name: string; children: ReactNode }> = ({
+  name,
+  children,
+}) => (
+  <Flex align="baseline" gap="small" style={{ margin: 3 }}>
+    <Typography.Text strong>{name}</Typography.Text>
+    {children}
+  </Flex>
+);
